9048c40bd677096300a7ad26fc5043428f5b6d25
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-05 13:38:04 +0200

    1.1 read file

--- src/javastreams/JavaStreams.java
+++ src/javastreams/JavaStreams.java
@@ -5,6 +5,13 @@
  */
 package javastreams;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author vmatheis
@@ -14,8 +21,21 @@ public class JavaStreams {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        List<Weapon> weapon = new ArrayList<>();
+
+        weapon = Files.lines(new File("weapons.csv").toPath()).skip(1)
+                .map(s -> s.split(";"))
+                .map(s -> new Weapon(
+                s[0],
+                CombatType.valueOf(s[1]),
+                DamageType.valueOf(s[2]),
+                Integer.parseInt(s[3]),
+                Integer.parseInt(s[4]),
+                Integer.parseInt(s[5]),
+                Integer.parseInt(s[6])
+                ))
+                .collect(Collectors.toList());
     }
-    
+
 }
