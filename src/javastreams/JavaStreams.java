7253f72e95b5142e83fdbbb5aaa3b85c64a026df
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-05 14:13:06 +0200

    1.2 Comparator

--- src/javastreams/JavaStreams.java
+++ src/javastreams/JavaStreams.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +19,9 @@ import java.util.stream.Collectors;
  */
 public class JavaStreams {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
+    static Comparator<Weapon> c;
+
+    public static List<Weapon> main(String[] args) throws IOException {
         List<Weapon> weapon = new ArrayList<>();
 
         weapon = Files.lines(new File("weapons.csv").toPath()).skip(1)
@@ -34,8 +34,13 @@ public class JavaStreams {
                 Integer.parseInt(s[4]),
                 Integer.parseInt(s[5]),
                 Integer.parseInt(s[6])
-                ))
+        ))
                 .collect(Collectors.toList());
+        weapon.sort(c);
+        return weapon;
     }
 
+    public static void initialize() {
+        c = ((o1, o2) -> Integer.compare(o1.getDamage(), o2.getDamage()));
+    }
 }
