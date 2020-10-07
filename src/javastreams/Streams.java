2afa21da223227ea2495606f7b70c2736202f57f
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-07 21:41:52 +0200

    2.6 Methoden-Streams

--- src/javastreams/Streams.java
+++ src/javastreams/Streams.java
@@ -15,17 +15,18 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Streams {
-    
+
     int[] numbers;
     String[] string = new String[10];
     List<Weapon> weapon = new ArrayList<>();
-    
-    public Streams(int[] numbers) {
+
+    public Streams(int[] numbers) throws IOException {
         this.numbers = numbers;
         for (int i = 0; i < 1000; i++) {
             this.numbers[i] = ((int) ((Math.random() * 100) + 1));
@@ -35,56 +36,57 @@ public class Streams {
             new Random().nextBytes(b);
             s = new String(b, Charset.forName("UTF-8"));
         }
+        readFile();
     }
-    
+
     public double average(int[] numbers) {
         return Arrays.stream(numbers).asDoubleStream().average().getAsDouble();
     }
-    
+
     public List<String> upperCase(String[] strings) {
         return Arrays.stream(string).map(w -> w.toUpperCase()).collect(Collectors.toList());
     }
-    
+
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().min(Comparator.comparing(Weapon::getDamage)).get();
     }
-    
+
     public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().max(Comparator.comparing(Weapon::getMinStrength)).get();
     }
-    
+
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().filter(w -> w.getDamageType() == DamageType.MISSILE).collect(Collectors.toList());
     }
-    
+
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().max(Comparator.comparing(Weapon::getName)).get();
     }
-    
+
     public List<String> toNameList(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().map(w -> w.getName()).collect(Collectors.toList());
     }
-    
+
     public int[] toSpeedArray(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().mapToInt(w -> w.getSpeed()).toArray();
     }
-    
+
     public int sumUpValues(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().mapToInt(w -> w.getSpeed()).sum();
     }
-    
+
     public long sumUpHashCodes(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().mapToInt(w -> w.hashCode()).sum();
     }
-    
+
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
-        //implement this
+        return weapons.stream().map(w -> w).collect(Collectors.toList());
     }
-    
+
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
-        //implement this
+        weapons.stream().map(w -> w.setValue(((int) w.getValue() * 1.1)));
     }
-    
+
     public void readFile() throws IOException {
         weapon = Files.lines(new File("weapons.csv").toPath()).skip(1)
                 .map(s -> s.split(";"))
