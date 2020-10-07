54a4f5c33c7ebe492d6807e3cbbed0a25be7822a
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-07 20:33:33 +0200

    2.1 Array - Random

---
+++ src/javastreams/Streams.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastreams;
+
+/**
+ *
+ * @author vmatheis
+ */
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Streams {
+    int[] numbers;
+
+    public Streams(int[] numbers) {
+        this.numbers = numbers;
+        for (int i = 0; i < 1000; i++) {
+            this.numbers[i] = ((int) ((Math.random()*100)+1));
+        }
+    }
+    
+    
+
+    public double average(int[] numbers) {
+        //implement this
+    }
+
+    public List<String> upperCase(String[] strings) {
+        //implement this
+    }
+
+    public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public List<String> toNameList(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public int[] toSpeedArray(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public int sumUpValues(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public long sumUpHashCodes(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public List<Weapon> removeDuplicates(List<Weapon> weapons) {
+        //implement this
+    }
+
+    public void increaseValuesByTenPercent(List<Weapon> weapons) {
+        //implement this
+    }
+}
