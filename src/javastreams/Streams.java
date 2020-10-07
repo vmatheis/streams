a6db5a599abfafddb68a1c8597293713dfadc666
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-07 21:00:14 +0200

    2.5 readFile

--- src/javastreams/Streams.java
+++ src/javastreams/Streams.java
@@ -9,20 +9,26 @@ package javastreams;
  *
  * @author vmatheis
  */
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Streams {
+
     int[] numbers;
     String[] string = new String[10];
+    List<Weapon> weapon = new ArrayList<>();
 
     public Streams(int[] numbers) {
         this.numbers = numbers;
         for (int i = 0; i < 1000; i++) {
-            this.numbers[i] = ((int) ((Math.random()*100)+1));
+            this.numbers[i] = ((int) ((Math.random() * 100) + 1));
         }
         byte[] b = new byte[10];
         for (String s : string) {
@@ -77,5 +83,20 @@ public class Streams {
 
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
         //implement this
+    }
+    
+    public void readFile() throws IOException{
+         weapon = Files.lines(new File("weapons.csv").toPath()).skip(1)
+                .map(s -> s.split(";"))
+                .map(s -> new Weapon(
+                s[0],
+                CombatType.valueOf(s[1]),
+                DamageType.valueOf(s[2]),
+                Integer.parseInt(s[3]),
+                Integer.parseInt(s[4]),
+                Integer.parseInt(s[5]),
+                Integer.parseInt(s[6])
+        ))
+                .collect(Collectors.toList());
     }
 }
