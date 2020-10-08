b65c74378ce2b407eaae5bf35143c24c308538fc
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-10-08 15:07:53 +0200

    4. odd numbers sums

--- src/javastreams/FilterOdd.java
+++ src/javastreams/FilterOdd.java
@@ -14,5 +14,8 @@ import java.util.stream.IntStream;
  * @author vmatheis
  */
 public class FilterOdd {
-    
+    static final int result = IntStream.of(1,2,3,4,5,6,7,8,9,10).filter(w -> w%2 != 0).map(x -> x*x).sum();
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
 }
